528589a6bc59eef8921a90eb57530009ddf03051
jokrhub <loganalex007@gmail.com>
2020-12-28 22:32:34 +0530

    added register functionality

--- app/admin/models.py
+++ app/admin/models.py
@@ -1,18 +1,18 @@
 from enum import unique
+from typing import Tuple
 from app import db
 from sqlalchemy import Column, String, SmallInteger, DateTime
 
 class Base(db.Model):
     __abstract__ = True
-    id = db.Column(String(7), primary_key=True)
+    sid = db.Column(String(7), primary_key=True)
     date_created = db.Column(DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
 
 class User(Base):
-    sid = db.Column(String(128), nullable=False, unique=True)
-    name = db.Column(String(128), nullable=False)
-    email = db.Column(String(128), nullable=False, unique=True)
+    name = db.Column(String(128), nullable=True)
+    email = db.Column(String(128), nullable=True, unique=True)
     password = db.Column(String(192), nullable=False)
     role = db.Column(SmallInteger, nullable=False)
 
@@ -24,6 +24,11 @@ class User(Base):
     def __repr__(self):
         return '<User %r>' % (self.name)
 
+user = User.query.filter_by(sid="N170076").first()
+if not user:
+    user = User("N170076", "p@ss", 1)
+    db.session.add(user)
+    db.session.commit()
 
 
 
