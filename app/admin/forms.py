f4d1c527f9ccb8f24dcba02771e2f794ec67ed17
sh1va-shankar <n170295@rguktn.ac.in>
2020-12-29 14:16:10 -0800

    added login_manager to authenticate and added a dashboard(sample) also modified the register form according to the hirarachy

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -6,15 +6,15 @@ from wtforms import TextField, PasswordField, IntegerField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    sid = TextField('Student ID', [
+    id = TextField('Student ID', [
                 Required(message='Forgot your college ID')])
     password = PasswordField('Password', [
                 Required(message='Must provide a password. ;-)')])
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    sid = TextField('Student ID', [
-                Required(message='Forgot your college ID')])
+    id = TextField('Student ID', [
+                Required(message='Forgot your college ID')])    
     password = PasswordField('Password', [
                 Required(message='Must provide a password. ;-)')])
     role = IntegerField('Role', [
--- app/admin/models.py
+++ app/admin/models.py
@@ -2,10 +2,11 @@ from enum import unique
 from typing import Tuple
 from app import db
 from sqlalchemy import Column, String, SmallInteger, DateTime
+from flask_login import UserMixin
 
-class Base(db.Model):
+class Base(UserMixin,db.Model):
     __abstract__ = True
-    sid = db.Column(String(7), nullable=False, primary_key=True)
+    id = db.Column(String(7), nullable=False, primary_key=True)
     date_created = db.Column(DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
@@ -16,22 +17,23 @@ class User(Base):
     password = db.Column(String(192), nullable=False)
     role = db.Column(SmallInteger, nullable=False)
 
-    def __init__(self, sid, password, role):
-        self.sid = sid
+    def __init__(self, id, password, role):
+        self.id = id
         self.role = role
         self.password = password
 
     def __repr__(self):
         return '<User %r>' % (self.name)
 
-user = User.query.filter_by(sid="N170076").first()
+
+db.create_all()
+
+user = User.query.filter_by(id="N170076").first()
 
 if not user:
     user = User("N170076", "p@ss", 1)
     db.session.add(user)
     db.session.commit()
-
-
 
 
  
