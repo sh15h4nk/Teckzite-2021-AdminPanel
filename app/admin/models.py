48a6ab2ab2548adad85811046dba82e149fff7d9
jokrhub <loganalex007@gmail.com>
2021-02-04 11:11:40 +0530

    Updated schema

--- app/admin/models.py
+++ app/admin/models.py
@@ -1,8 +1,14 @@
 from enum import unique
 from typing import Tuple
+
+from sqlalchemy.orm import backref
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Boolean, Integer
+
 from app import db
 from sqlalchemy import Column, String, SmallInteger, DateTime
 from flask_login import UserMixin
+
 
 class Base(UserMixin,db.Model):
     __abstract__ = True
@@ -17,6 +23,14 @@ class User(Base):
     password = db.Column(String(192), nullable=False)
     role = db.Column(SmallInteger, nullable=False)
 
+    event_organised_id = db.Column(Integer, ForeignKey('event.id'))
+    event_coordinated_id = db.Column(Integer, ForeignKey("event.id"))
+    
+    workshop_organised_id = db.Column(Integer, ForeignKey('workshop.id'))
+    workshop_coordinated_id = db.Column(Integer, ForeignKey("workshop.id"))
+
+
+
     def __init__(self, id, password, role):
         self.id = id
         self.role = role
@@ -25,6 +39,55 @@ class User(Base):
     def __repr__(self):
         return '<User %r>' % (self.name)
 
+
+class Event(Base):
+    name = db.Column(String(128), nullable=False)
+    
+    details = db.Column(String(256))
+    teamsize = db.Column(SmallInteger, nullable=False)
+    organiser = db.relationship("User", backref="org_event", foreign_keys=[User.event_organised_id], uselist=False)         # backreference from User to retiieve user hosted event ORGANISER
+    coordinator =  db.relationship("User", backref="cord_event",foreign_keys=[User.event_coordinated_id], uselist=False)     # backreference from User to retiieve user hosted event COORDINATOR
+    # teams = db.relationship("Team", backref="event")
+
+class Workshop(Base):
+    name = db.Column(String(128), nullable=False)
+    
+    details = db.Column(String(256))
+    organiser = db.relationship("User", backref="org_workshop" ,foreign_keys=[User.workshop_organised_id] ,uselist=False)
+    coordinator =  db.relationship("User", backref="cord_workshop", foreign_keys=[User.workshop_coordinated_id], uselist=False)
+
+#     tech_user_id = db.Column(String(256), ForeignKey('techUser.id'))
+
+
+
+
+# class TechUser(Base):
+#     name = db.Column(String(128), nullable=False)
+#     email = db.Column(String(128), nullable=True, unique=True)
+#     password = db.Column(String(192), nullable=False)
+#     dept = db.Column(String(128))
+#     payment = db.Column(Boolean, nullable=False)
+    
+#     workshops = db.relationship('Workshop', backref='techUser')
+
+#     team_id = db.Column(db.Integer, ForeignKey('team.id'))
+
+#     event_organiser_id = db.Column(db.Integer, ForeignKey('event.id'))
+#     # event_coordinator_id = db.Column(db.Integer, ForeignKey('event.id'))
+
+#     # workshop_coordinator_id = db.Column(db.Integer, ForeignKey('workshop.id'))
+#     workshop_organiser_id = db.Column(db.Integer, ForeignKey('workshop.id'))
+
+#     team_id = db.Column(db.Integer, ForeignKey('team.id'))
+
+
+
+
+
+# class Team(Base):
+#     techUsers = db.relationship('TechUser', backref='team')
+
+#     event_id = db.Column(Integer, ForeignKey('event.id'))
 
 db.create_all()
 
