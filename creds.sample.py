83918d9203945ee45c739cb9f472ad019c0cb91b
sh15h4nk <shiva.shvvs@gmail.com>
2021-08-23 22:18:16 +0530

    addedd sample creds
    Signed-off-by: sh15h4nk <shiva.shvvs@gmail.com>

---
+++ creds.sample.py
@@ -0,0 +1,26 @@
+#localhost
+# dbuser="root"
+# dbpasswd="XXXXX"
+# dbhost="localhost"
+# dbname="tz2021"
+
+#csrf
+csrf_key = ""
+
+#app
+secret_key = ""
+
+
+#Email server
+email_username = ""
+email_passwd = ""
+
+mail_server = ""
+mail_port = 
+
+
+#AWS RDS SQL CREDS
+dbuser=""
+dbpasswd=""
+dbhost=""
+dbname=""
